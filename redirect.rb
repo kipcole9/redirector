7164657a55541288f5d74a5d726d146281e6e650
Kip Cole <kip@Kip-macbook.local>
2010-06-18 01:30:02 +0800

    Make sure we copy over query params when redirecting

--- redirect.rb
+++ redirect.rb
@@ -8,9 +8,8 @@ end
 
 get '/r/:id' do
   if redirect_to = Redirect.find_by_redirect_url(params[:id])
-    redirect redirect_to.url
+    redirect request.query_string ? "#{redirect_to.url}?#{request.query_string}" : redirect_to.url
   else
     throw :halt, [404, "Sorry, don't know how to redirect with '#{params[:id]}'"]
   end
 end
-
